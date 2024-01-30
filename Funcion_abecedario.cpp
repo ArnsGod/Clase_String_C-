107a4fc57b30a01e9d365324f6257b6e3b211d1e
ArnsGod <155787083+ArnsGod@users.noreply.github.com>
2024-01-29 22:11:12 -0400

    Add files via upload

---
+++ Funcion_abecedario.cpp
@@ -0,0 +1,65 @@
+/*
+Titulo:Generar letras y numero aleatorios
+Auntor:Jose Eduardo Flores Saravia
+Descripcion: Ejercicio en clases
+Fecha 2024-01-29
+*/
+#include <iostream>
+
+using namespace std;
+
+char caracter[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int numero[26];
+char c[26];
+
+char generarCaracter()//Genera un caracter aleatorio a,b,c...
+{
+	return caracter[rand() % 26];
+}
+
+int generarNumero()//genera un numero aleatorio entre 0 - 50
+{
+	return rand() % 51;
+}
+
+void imprimir()//Imprime el caracter y el numero
+{
+	cout << "Car\tNUM" << endl;
+	for (int i = 0; i < 26; i++)
+		cout << c[i] << "\t" << numero[i] << endl;
+}
+
+void generarDatos()//genera el numero y el caracter aleatorio
+{
+	for (int i = 0; i < 26; i++)
+	{
+		numero[i] = generarNumero();
+		c[i] = generarCaracter();
+	}
+}
+
+void antDuplicado()//Funcion para que la caracter no se repita
+{
+	for (int i = 0; i < 26; i++)//bucle externo
+	{
+		for (int j = 0; j < i; j++)//bucle interno
+		{
+			//Generar una nueva letra con uso de un bucle while
+			while (c[i] == c[j])//Si la letra de la pocion i es igual a la letra de la pocision j entonces inicia este bucle
+			{
+				/*si se detecta una letra duplicada c[i] va a agenerar un nuevo caracter*/
+				c[i] = generarCaracter();
+				/*Una vez cambiado el caracter el contador interno c[j] se reinicia para verificar que no se generen caracteres identicos.*/
+				j = 0; // Reiniciar el bucle interno para verificar desde el principio
+			}
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	generarDatos();
+	antDuplicado();
+	imprimir();
+	return 0;
+}
---
+++ frutas.cpp
@@ -0,0 +1,79 @@
+/*
+Titulo:Generar fruta y preparado aleatorios
+Auntor:Jose Eduardo Flores Saravia
+Descripcion: Ejercicio en clases
+Fecha 2024-01-29
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+string frutas[] = {"Manzana", "Banana", "Naranja", "Fresa", "Kiwi", "Mango", "Pera", "Uva", "Melón", "Papaya"};//Cadema de fritas
+string preparados[] = {"Jugo", "Ensalada", "Frapé", "Tarta", "Helado", "Mermelada", "Batido", "Asado", "Cóctel", "Crudo"};//Cadena de preparado
+string fruta[10];//Almacenar la generacion de frutas en un orden aleatorio de la cadena 
+string preparado[10];//Almacenar la generacion de preparado en un orden aleatorio de la cadena
+
+string generarFruta()//genera la fruta de la cadena en un orden aleatorio
+{
+	return frutas[rand() % 10];
+}
+
+string generarPreparado()//genera el preparado de la cadena en un orden aleatorio
+{
+	return preparados[rand() % 10];
+}
+
+void generar()//Genere el orden de la generacion de fruta y preparado
+{
+	for (int i = 0; i < 10; i++)
+	{
+		fruta[i] = generarFruta();
+		preparado[i] = generarPreparado();
+	}
+}
+
+void imprimir()//imprime la fruta y el preparado
+{
+	cout << "Menu de productos" << endl;
+	for (int i = 0; i < 10; i++)
+		cout << preparado[i] << " de " << fruta[i] << endl;
+}
+
+void antDuplicadoFru()//Funcion para que la fruta no se duplique
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			while (fruta[i] == fruta[j])
+			{
+				fruta[i] = generarFruta();
+				j = 0;
+			}
+		}
+	}
+}
+
+void antDuplicadoPre()//Funcion para que la preparado no se duplique
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			while (preparado[i] == preparado[j])
+			{
+				preparado[i] = generarPreparado();
+				j = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	generar();
+	antDuplicadoFru();
+	antDuplicadoPre();
+	imprimir();
+	return 0;
+}
